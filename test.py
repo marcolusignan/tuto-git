b03f3f7ce898a578bf8a55c179ae8c6b21768091
marcolusignan <marco.lusignan@gmail.com>
2020-03-17 11:53:25 +0100

    message ammended

--- test.py
+++ test.py
@@ -8,3 +8,5 @@
 # git log : affiche l'historique des commit, ligne du haut : dernier commit réalisé, ligne du bas : 1er commit
 # infos visibles du log : SHA : id unique du commit / auteur / date du commit / message
 # la touche q permet de quitter le log
+
+# git commit -a -m "message" : met a jour tous les fichiers existants préalablement ajoutés à l'index
