3cc81db01391178a567140c7762a59052e262b63
marcolusignan <marco.lusignan@gmail.com>
2020-03-17 11:25:26 +0100

    4e message

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
 # git init : initialise le dossier courant comme repository git
+
+# git add nomfichier : ajoute un fichier à l'index du repository
+# git add . : ajoute tous les fichiers du dossier courant à l index
+
+# git commit -m "message": enregistre les modifs du repository et ajoute un message
+
+# git log : affiche l'historique des commit, ligne du haut : dernier commit réalisé, ligne du bas : 1er commit
+# infos visibles du log : SHA : id unique du commit / auteur / date du commit / message
+# la touche q permet de quitter le log
